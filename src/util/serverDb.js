832ac5e1f6d1315a2837726d4f5ac4e1ca55a058
Utkarsh Bhimte <Utkarshbhimte@users.noreply.github.com>
2020-07-04 15:35:51 +0530

    Added delete work functionality; added server db; (#34)
    * fix: removing other auth types for now; minor bugs
    '
    
    * fix: added delete work functionality
    
    * fix: added server db

--- src/util/serverDb.js
+++ src/util/serverDb.js
@@ -1,7 +1,31 @@
-const { firestore } = require("./db");
+import admin from "firebase-admin";
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  });
+} catch (error) {
+  /*
+   * We skip the "already exists" message which is
+   * not an actual error when we're hot-reloading.
+   */
+  if (!/already exists/u.test(error.message)) {
+    // eslint-disable-next-line no-console
+    console.error("Firebase admin initialization error", error.stack);
+  } else {
+    console.error(error);
+  }
+}
+
+const db = admin.firestore();
 
 export const getWorkDetailsFromServer = async (workId) => {
-  const snapshot = await firestore.doc(`works/${workId}`).get();
-  const data = snapshot.data();
+  const doc = await db.doc(`works/${workId}`).get();
+  const data = doc.data();
   return data;
 };
