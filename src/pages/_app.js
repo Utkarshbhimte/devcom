003f5e5e43c473ccfe7c1b523e95085b3ec66cc3
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2020-07-04 21:57:47 +0530

    fix: added sentry

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,33 +1,65 @@
 import React from "react";
+import * as Sentry from "@sentry/browser";
+
 import "styles/global.scss";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 import "util/analytics.js";
 import { ProvideAuth } from "util/auth.js";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ProvideAuth>
-      <>
-        <Navbar
-          color="white"
-          spaced={true}
-          logo="https://uploads.divjoy.com/logo.svg"
-        ></Navbar>
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  });
+}
+class MyApp extends React.Component {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
-        <Component {...pageProps} />
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
 
-        <Footer
-          color="light"
-          size="normal"
-          backgroundImage=""
-          backgroundImageOpacity={1}
-          copyright="© 2019 Company"
-          logo="https://uploads.divjoy.com/logo.svg"
-        ></Footer>
-      </>
-    </ProvideAuth>
-  );
+    return { pageProps };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope((scope) => {
+      Object.keys(errorInfo).forEach((key) => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+
+      Sentry.captureException(error);
+    });
+
+    super.componentDidCatch(error, errorInfo);
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ProvideAuth>
+        <>
+          <Navbar
+            color="white"
+            spaced={true}
+            logo="https://uploads.divjoy.com/logo.svg"
+          ></Navbar>
+
+          <Component {...pageProps} />
+
+          <Footer
+            color="light"
+            size="normal"
+            backgroundImage=""
+            backgroundImageOpacity={1}
+            copyright="© 2019 Company"
+            logo="https://uploads.divjoy.com/logo.svg"
+          ></Footer>
+        </>
+      </ProvideAuth>
+    );
+  }
 }
 
 export default MyApp;
