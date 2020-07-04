dc9cc49d823c1e2fb1835adccee54740818ccb2e
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2020-07-04 21:41:38 +0530

    fix: added sentry

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withSass = require("@zeit/next-sass");
 const withImages = require("next-images");
+const withSourceMaps = require("@zeit/next-source-maps")();
+
 // Extend your Next config for advanced behavior
 // See https://nextjs.org/docs/api-reference/next.config.js/introduction
 let nextConfig = {};
 
 // Add the Next SASS plugin
-nextConfig = withSass(withImages(nextConfig));
+nextConfig = withSourceMaps(withSass(withImages(nextConfig)));
 
 module.exports = nextConfig;
