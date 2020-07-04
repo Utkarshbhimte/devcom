df1386d9359d1027ff105f40db14d6b3d5e74ba4
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2020-07-04 14:28:12 +0530

    feat: comment functionality; resolved #3

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./CommentSection.scss";
-import { useComments, useUser } from "util/db";
+import { useComments, useUser, firestore, updateComment } from "util/db";
 import { formatDate } from "util/date";
 
+const loadingState = {
+  delete: "Delete",
+  edit: "Edit",
+  comment: "Comment",
+};
 const CommentCell = ({ comment }) => {
-  const { data: authorData } = useUser(comment.author);
-  console.log("CommentCell -> authorData", authorData);
+  const { data: authorData } = useUser(comment.owner);
+  const inputRef = useRef(null);
+  const [loading, setLoading] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const editComment = () => setEditMode(true);
+  const deleteComment = async () => {
+    if (!!loading) {
+      return;
+    }
+
+    setLoading(loadingState.delete);
+
+    try {
+      await firestore
+        .doc(["works", comment.workId, "comments", comment.id].join("/"))
+        .delete();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(null);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event && event.preventDefault();
+    const text = inputRef.current && inputRef.current.value;
+
+    if (!(text && !!text.length)) {
+      deleteComment();
+      return;
+    }
+    inputRef.current.value = "";
+
+    try {
+      setLoading(loadingState.edit);
+      setEditMode(false);
+      await updateComment({
+        workId: comment.workId,
+        text,
+        commentId: comment.id,
+      });
+    } catch (error) {
+      setEditMode(true);
+      console.error(error);
+      inputRef.current.value = text;
+    } finally {
+      setLoading(null);
+    }
+  };
+
+  const handleEnterPressOnInput = (e) => {
+    if (e.keyCode == 13 && e.shiftKey == false) {
+      e.preventDefault();
+      handleSubmit();
+    }
+  };
 
   return (
     <div className="comment-row">
@@ -16,14 +76,54 @@ const CommentCell = ({ comment }) => {
       )}
       <div>
         {authorData && (
-          <div>
-            <strong>{authorData.displayName}</strong> |{" "}
-            {comment.created && (
-              <small>{formatDate(comment.created.seconds * 1000)}</small>
-            )}
+          <div className="comment-header">
+            <strong>{authorData.displayName}</strong>
           </div>
         )}
-        <p className="comment-content">{comment.text}</p>
+        {!editMode && <p className="comment-content">{comment.text}</p>}
+        {!!editMode && (
+          <form className="edit-input-wrapper" onSubmit={handleSubmit}>
+            <textarea
+              ref={inputRef}
+              defaultValue={comment.text}
+              onKeyDown={handleEnterPressOnInput}
+              className={`textarea comment-input`}
+              placeholder="Share your thoughts on this project"
+            ></textarea>
+            <button
+              className="primary-action"
+              type="submit"
+              className={`button is-primary ${
+                loading === loadingState.edit ? "is-loading" : ""
+              }`}
+            >
+              Update
+            </button>
+          </form>
+        )}
+        <div className="comment-actions">
+          <a
+            className=" comment-action-btn"
+            role="button"
+            onClick={editComment}
+          >
+            Edit
+          </a>
+          <a
+            className={`delete-btn comment-action-btn ${
+              loading === loadingState.delete ? "is-loading" : ""
+            }`}
+            role="button"
+            onClick={deleteComment}
+          >
+            Delete
+          </a>
+          {comment.created && (
+            <small className="timestamp">
+              {formatDate(comment.created.seconds * 1000)}
+            </small>
+          )}
+        </div>
       </div>
     </div>
   );
@@ -33,7 +133,10 @@ const CommentSection = ({ workId }) => {
   const { data } = useComments(workId);
   return (
     <div className="comment-section">
-      {data && data.map((comment) => <CommentCell comment={comment} />)}
+      {data &&
+        data.map((comment) => (
+          <CommentCell key={comment.id} comment={comment} />
+        ))}
     </div>
   );
 };
--- src/components/WorkPageComponent/WorkPageComponent.jsx
+++ src/components/WorkPageComponent/WorkPageComponent.jsx
@@ -1,55 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./WorkPage.scss";
-import { useAuth } from "util/auth";
-import { createComment } from "util/db";
 import CommentSection from "components/CommentSection/CommentSection";
-
-const CommentInput = ({ workId }) => {
-  const { user } = useAuth();
-  const inputRef = useRef();
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const text = inputRef.current && inputRef.current.value;
-
-    if (!(text && !!text.length)) {
-      return;
-    }
-    inputRef.current.value = "";
-
-    try {
-      setLoading(true);
-      await createComment(workId, text, user.uid);
-    } catch (error) {
-      console.error(error);
-      inputRef.current.value = text;
-    } finally {
-      setLoading(false);
-    }
-  };
-  return user ? (
-    <form className="input-wrapper" onSubmit={handleSubmit}>
-      {user && (
-        <img className="avatar" src={user.photoURL} alt="Placeholder image" />
-      )}
-      <textarea
-        ref={inputRef}
-        className={`textarea comment-input`}
-        placeholder="Share your thoughts on this project"
-      ></textarea>
-      <button
-        className="primary-action"
-        type="submit"
-        className={`button is-primary ${loading ? "is-loading" : ""}`}
-      >
-        Send
-      </button>
-    </form>
-  ) : (
-    <div className="primary-action">Login now to join the discussion</div>
-  );
-};
+import { CommentInput } from "./CommentInput";
 
 const WorkPageComponent = ({ data }) => {
   return (
