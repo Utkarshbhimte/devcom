ef25ea0916683be8b0056db6ad68c1766890b8ae
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-07-15 16:18:47 +0530

    added loading skeleton for dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,16 +1,17 @@
-import React from "react";
-import DashboardPlaceholder from "components/DashboardPlaceholder";
+import React, { lazy, Suspense } from "react";
 import { requireAuth } from "util/auth.js";
+import DashboardLoadingSkeleton from "../components/LoadingSkeleton/DashboardLoadingSkeleton";
+
+const Dashboard = lazy(() => import("../components/DashboardPlaceholder"));
 
 function DashboardPage(props) {
-  return (
-    <DashboardPlaceholder
-      color="white"
-      size="large"
-      title="Dashboard"
-      subtitle="Dashboard components are coming to the Divjoy library soon. For now, you can implement a custom dashboard here after exporting your code."
-    ></DashboardPlaceholder>
-  );
+    return (
+        <>
+            <Suspense fallback={<DashboardLoadingSkeleton />}>
+                <Dashboard />
+            </Suspense>
+        </>
+    );
 }
 
 export default requireAuth(DashboardPage);
